1d9008d6296594d436d6ac4470c1514e98e4d116
AndersAarsaether <584975@stud.hvl.no>
2023-11-21 10:20:07 +0100

    Removed two more folders

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import initializeAdvocaAPI from './apis/advoca.api'
 import initializePartnerAPI from './apis/partner.api'
 
+//Empty line between compile messages and runtime messages
+console.log()
+
 initializeAdvocaAPI()
 initializePartnerAPI()

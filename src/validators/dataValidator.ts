3ff8477309a6310aefb1444d47236cba70d1499b
AndersAarsaether <584975@stud.hvl.no>
2024-01-18 11:26:55 +0100

    Updated datavalidator error handling

--- src/validators/dataValidator.ts
+++ src/validators/dataValidator.ts
@@ -7,18 +7,30 @@ import { ErrorResponse } from '../types/responses'
 const dataValidator = (schema: z.ZodSchema) => {
     return (req: Request, res: Response, next: NextFunction) => {
         try {
+            // Try to parse the body into schema-typed object
             schema.parse(req.body)
+            // If there are no errors, proceed to next middleware function
             next()
         } catch (error) {
+            // Ensure it is a typing error
             if (error instanceof ZodError) {
+                // Explain why it failed in the response
                 const response: ErrorResponse = {
                     message: 'The body did not conform to the schema',
                     errors: getZodTypeErrors(error),
                 }
+                // Send the response back to sender
                 res.status(400).json(response)
                 return
             }
-            next(error)
+            // Something else failed
+            const description = error?.message as string
+            const response: ErrorResponse = {
+                message: 'Something went wrong',
+                errors: [description],
+            }
+            res.status(500).json(response)
+            return
         }
     }
 }
